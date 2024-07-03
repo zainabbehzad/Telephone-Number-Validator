041bb441d7e9eb4274de73bb0751219c9850801e
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-07-03 12:37:10 +0430

    fixed ESLint linters errors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const userInput = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
@@ -30,6 +31,59 @@ const checkValidNumber = (input) => {
 };
 
 checkBtn.addEventListener('click', () => {
+  checkValidNumber(userInput.value);
+  userInput.value = '';
+});
+
+userInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    checkValidNumber(userInput.value);
+    userInput.value = '';
+  }
+});
+
+clearBtn.addEventListener('click', () => {
+  resultsDiv.textContent = '';
+});
+*/
+
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultsDiv = document.getElementById('results-div');
+
+const countryCode = '^(1\\s?)?';
+const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+const spacesDashes = '[\\s\\-]?';
+const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+const phoneRegex = new RegExp(
+  `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+);
+
+const checkValidNumber = (input) => {
+  resultsDiv.innerHTML = ''; // Clear the previous results
+
+  if (input === '') {
+    const pTag = document.createElement('p');
+    pTag.className = 'results-text';
+    pTag.style.color = '#4d3800';
+    pTag.textContent = 'Please provide a phone number';
+    resultsDiv.appendChild(pTag);
+    return;
+  }
+
+  const pTag = document.createElement('p');
+  pTag.className = 'results-text';
+  pTag.style.color = phoneRegex.test(input) ? '#00471b' : '#4d3800';
+  pTag.textContent = `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`;
+  resultsDiv.appendChild(pTag);
+};
+
+checkBtn.addEventListener('click', () => {
+  if (userInput.value === '') {
+    alert('Please provide a phone number');
+    return;
+  }
   checkValidNumber(userInput.value);
   userInput.value = '';
 });
