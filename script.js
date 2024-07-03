339613fd13c6ede551a476750791b4669f068b20
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-07-03 12:25:03 +0430

    fixed ESLint linters errors

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ const checkValidNumber = (input) => {
   const spacesDashes = '[\\s\\-]?';
   const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
   const phoneRegex = new RegExp(
-    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`,
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
   );
-
+  
   const pTag = document.createElement('p');
   pTag.className = 'results-text';
-  phoneRegex.test = (input)
+  phoneRegex.test(input)
     ? (pTag.style.color = '#00471b')
     : (pTag.style.color = '#4d3800');
   pTag.appendChild(
     document.createTextNode(
-      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`,
-    ),
+      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`
+    )
   );
   resultsDiv.appendChild(pTag);
 };
