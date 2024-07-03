72581762e2e154cef06fda96bf91c7eb34f193b7
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-07-03 12:41:29 +0430

    Fixed some functions doesn't work correctly in js

--- script.js
+++ script.js
@@ -1,52 +1,3 @@
-/*
-const userInput = document.getElementById('user-input');
-const checkBtn = document.getElementById('check-btn');
-const clearBtn = document.getElementById('clear-btn');
-const resultsDiv = document.getElementById('results-div');
-
-const checkValidNumber = (input) => {
-  if (input === '') {
-    alert('Please provide a phone number');
-    return;
-  }
-  const countryCode = '^(1\\s?)?';
-  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
-  const spacesDashes = '[\\s\\-]?';
-  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
-  const phoneRegex = new RegExp(
-    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
-  );
-  
-  const pTag = document.createElement('p');
-  pTag.className = 'results-text';
-  phoneRegex.test(input)
-    ? (pTag.style.color = '#00471b')
-    : (pTag.style.color = '#4d3800');
-  pTag.appendChild(
-    document.createTextNode(
-      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`
-    )
-  );
-  resultsDiv.appendChild(pTag);
-};
-
-checkBtn.addEventListener('click', () => {
-  checkValidNumber(userInput.value);
-  userInput.value = '';
-});
-
-userInput.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter') {
-    checkValidNumber(userInput.value);
-    userInput.value = '';
-  }
-});
-
-clearBtn.addEventListener('click', () => {
-  resultsDiv.textContent = '';
-});
-*/
-
 const userInput = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
@@ -57,7 +8,7 @@ const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
 const spacesDashes = '[\\s\\-]?';
 const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
 const phoneRegex = new RegExp(
-  `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+  `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`,
 );
 
 const checkValidNumber = (input) => {
