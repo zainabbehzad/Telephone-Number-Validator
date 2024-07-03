20859124b12d03b208ed58a992f47cb032dbacf7
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-07-03 12:04:33 +0430

    fixed ESLint errors

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const resultsDiv = document.getElementById('results-div');
 
-const checkValidNumber = input => {
+const checkValidNumber = (input) => {
   if (input === '') {
     alert('Please provide a phone number');
     return;
@@ -13,18 +13,18 @@ const checkValidNumber = input => {
   const spacesDashes = '[\\s\\-]?';
   const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
   const phoneRegex = new RegExp(
-    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`,
   );
 
   const pTag = document.createElement('p');
   pTag.className = 'results-text';
-  phoneRegex.test(input)
+  phoneRegex.test = (input)
     ? (pTag.style.color = '#00471b')
     : (pTag.style.color = '#4d3800');
   pTag.appendChild(
     document.createTextNode(
-      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input}`
-    )
+      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} US number: ${input},`
+    ),
   );
   resultsDiv.appendChild(pTag);
 };
@@ -34,7 +34,7 @@ checkBtn.addEventListener('click', () => {
   userInput.value = '';
 });
 
-userInput.addEventListener('keydown', e => {
+userInput.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     checkValidNumber(userInput.value);
     userInput.value = '';
